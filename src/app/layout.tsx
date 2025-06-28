7a8a9f45595ac3cf63e312784892d2559049b3f1
treasure56 <simplicitytreasure@gmail.com>
2025-06-28 22:59:19 +0100

    Updated reviews

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Outfit, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfit = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} ${outfit.className} antialiased scroll-smooth`}
       >
         <Navbar />
         {children}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,9 +6,9 @@ export default function Navbar() {
   return (
     <header className="h-16 w-full sticky top-0 bg-white/90 z-30 flex  items-center app-container justify-between py-2 border-b border-gray-100 backdrop-blur-sm  gap-4">
       <div className="flex gap-4 h-full items-center text-brand-primary">
-        <p className="text-2xl font-bold text-brand-primary">
+        <Link href={paths.home} className="text-2xl font-bold text-brand-primary">
           Ca<span className="text-brand-secondary">see</span>
-        </p>
+        </Link>
       </div>
       <nav className="flex gap-4 h-full items-center">
         <div className="flex gap-4 border-r border-gray-100 pr-4 items-center max-md:hidden">
