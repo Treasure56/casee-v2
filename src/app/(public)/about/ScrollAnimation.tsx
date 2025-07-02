6a1ebbede50f5acec78321bdaed3bd2026a8e310
treasure56 <simplicitytreasure@gmail.com>
2025-07-02 21:21:08 +0100

    Designed upload page

--- src/app/(public)/about/ScrollAnimation.tsx
+++ src/app/(public)/about/ScrollAnimation.tsx
@@ -1,68 +1,68 @@
-"use client";
+// "use client";
 
-import SlideIn from "@/components/animation/SlideIn";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef } from "react";
+// import SlideIn from "@/components/animation/SlideIn";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { useRef } from "react";
 
-export default function ScrollAnimation() {
-  const h1Ref = useRef<HTMLHeadingElement>(null);
+// export default function ScrollAnimation() {
+//   const h1Ref = useRef<HTMLHeadingElement>(null);
 
-  useGSAP(() => {
-    gsap.registerPlugin(ScrollTrigger);
+//   useGSAP(() => {
+//     gsap.registerPlugin(ScrollTrigger);
 
-    ScrollTrigger.create({
-      trigger: h1Ref.current,
-      start: "top 20%",
-      end: "bottom top",
-      markers: true,
-      //   scrub: 3000,
-      onUpdate(st) {
-        const y = st.progress * 500;
-        // console.log(st.progress, y);
+//     ScrollTrigger.create({
+//       trigger: h1Ref.current,
+//       start: "top 20%",
+//       end: "bottom top",
+//       markers: true,
+//       //   scrub: 3000,
+//       onUpdate(st) {
+//         const y = st.progress * 500;
+//         // console.log(st.progress, y);
 
-        gsap.to(h1Ref.current, {
-          y: y,
-          //   y: 1 * (st.progress * 500),
-        });
-      },
-    });
+//         gsap.to(h1Ref.current, {
+//           y: y,
+//           //   y: 1 * (st.progress * 500),
+//         });
+//       },
+//     });
 
-    ScrollTrigger.create({
-      trigger: "#BlackBox",
-      start: "top 80%",
-      // end: "bottom top",
-      markers: true,
-      onEnter() {
-        gsap.to("#BlackBox", {
-          backgroundColor: "red",
-          opacity: 1,
-          scale: 1.5,
-        });
-      },
-      onLeaveBack() {
-        gsap.to("#BlackBox", {
-          backgroundColor: "black",
-          opacity: 0,
-          scale: 1,
-        });
-      },
-    });
-  });
+//     ScrollTrigger.create({
+//       trigger: "#BlackBox",
+//       start: "top 80%",
+//       // end: "bottom top",
+//       markers: true,
+//       onEnter() {
+//         gsap.to("#BlackBox", {
+//           backgroundColor: "red",
+//           opacity: 1,
+//           scale: 1.5,
+//         });
+//       },
+//       onLeaveBack() {
+//         gsap.to("#BlackBox", {
+//           backgroundColor: "black",
+//           opacity: 0,
+//           scale: 1,
+//         });
+//       },
+//     });
+//   });
 
-  return (
-    <div className="h-[200vh]">
-      <h1 ref={h1Ref} className="font-bold text-7xl">
-        TSMI
-      </h1>
+//   return (
+//     <div className="h-[200vh]">
+//       <h1 ref={h1Ref} className="font-bold text-7xl">
+//         TSMI
+//       </h1>
 
-      <div className="h-100"></div>
+//       <div className="h-100"></div>
 
-      <div id="BlackBox" className="rounded bg-black size-20 opacity-0"></div>
-      <SlideIn>
-        <p>hello</p>
-      </SlideIn>
-    </div>
-  );
-}
+//       <div id="BlackBox" className="rounded bg-black size-20 opacity-0"></div>
+//       <SlideIn>
+//         <p>hello</p>
+//       </SlideIn>
+//     </div>
+//   );
+// }
--- src/app/(public)/about/Simple.tsx
+++ src/app/(public)/about/Simple.tsx
@@ -1,17 +1,17 @@
-"use client"
-import gsap from "gsap";
-import { useRef } from "react";
+// "use client"
+// import gsap from "gsap";
+// import { useRef } from "react";
 
-export default function Page() {
-    const buttonRef = useRef<HTMLButtonElement>(null)
-    return (
-        <div>
-            <button ref={buttonRef} onClick={()=> gsap.fromTo(buttonRef.current, {
-                scale: 0,
-            }, {
-                scale: 1,
-                backgroundColor:"yellow"
-            })} className="btn-primary p-2 rounded">Hello</button>
-        </div>
-    );
-}
+// export default function Page() {
+//     const buttonRef = useRef<HTMLButtonElement>(null)
+//     return (
+//         <div>
+//             <button ref={buttonRef} onClick={()=> gsap.fromTo(buttonRef.current, {
+//                 scale: 0,
+//             }, {
+//                 scale: 1,
+//                 backgroundColor:"yellow"
+//             })} className="btn-primary p-2 rounded">Hello</button>
+//         </div>
+//     );
+// }
