5503e308ad56dd3cc4b85bbb426cba195054e979
treasure56 <simplicitytreasure@gmail.com>
2025-07-08 17:19:27 +0100

    fix: recent edits to option validators and design features

--- src/app/configure/design/DesignFeatures.tsx
+++ src/app/configure/design/DesignFeatures.tsx
@@ -1,5 +1,10 @@
 "use client";
-import { colors, finishes, material, models } from "@/app/validators/optionValidators";
+import {
+  colors,
+  finishes,
+  material,
+  models,
+} from "@/app/validators/optionValidators";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -13,6 +18,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { formatPrice } from "@/functions/helpers";
+import FormButton from "@/components/form/FormButton";
+import { basePrice } from "@/data/product";
 
 export default function DesignFeatures() {
   const [selectedColor, setSelectedColor] = useState<(typeof colors)[number]>(
@@ -25,9 +33,9 @@ export default function DesignFeatures() {
   const [selectedMaterial, setSelectedMaterial] = useState<
     (typeof material.options)[number]
   >(material.options[0]);
-  const [selectedFinish, setSelectedFinish] = useState<(typeof finishes)[number]>(
-    finishes.options[0]
-  )
+  const [selectedFinish, setSelectedFinish] = useState<
+    (typeof finishes)[number]
+  >(finishes.options[0]);
 
   return (
     <div className=" h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white">
@@ -101,23 +109,94 @@ export default function DesignFeatures() {
               </div>
 
               <div className="flex flex-col gap-3 w-full">
-                 <Label>
-                        {material.name}
+                <RadioGroup
+                  onValueChange={(val) => {
+                    const mtl = material.options.find((c) => c.value === val);
+                    if (mtl) setSelectedMaterial(mtl);
+                  }}
+                  value={selectedMaterial.value}
+                >
+                  <Label>{material.name}</Label>
+                  <div className="mt-3 flex flex-col items-center space-y-5 w-full">
+                    {material.options.map((mtl) => (
+                      <Label
+                        key={mtl.label}
+                        htmlFor={mtl.value}
+                        className={cn(
+                          "relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border border-zinc-200 sm:flex sm:justify-between w-full",
+                          {
+                            "border-brand-primary":
+                              selectedMaterial.value === mtl.value,
+                          }
+                        )}
+                      >
+                        <div className="flex flex-col text-sm">
+                          <span className="font-medium text-gray-900">
+                            {mtl.label}
+                          </span>
+                          {mtl.description && (
+                            <span className="text-gray-500">
+                              {mtl.description}
+                            </span>
+                          )}
+                        </div>
+                        <span className="text-sm font-medium text-gray-900">
+                          {formatPrice(mtl.price)}
+                        </span>
+                        <RadioGroupItem 
+                          value={mtl.value}
+                          id={mtl.value}
+                          className=" sr-only"
+                        />
                       </Label>
-                <RadioGroup value={selectedMaterial.value} onValueChange={(val) => {
-                  const materialOption = material.options.find((m) => m.value === val);
-                  if (materialOption) setSelectedMaterial(materialOption);
-                }}>
-                  <div className="mt-3 space-x-4">
-                  {
-                    material.options.map(mat =>(
-                      <RadioGroupItem key={mat.value} value={mat}>
-
-                      </RadioGroupItem>
-                    ))
-                  }
+                    ))}
                   </div>
-                 
+                </RadioGroup>
+
+                <RadioGroup
+                  onValueChange={(val) => {
+                    const finish = finishes.options.find((c) => c.value === val);
+                    if (finish) setSelectedFinish(finish);
+                  }}
+                  value={selectedFinish.value}
+                >
+                  <Label>{finishes.name}</Label>
+                  <div className="mt-3 flex flex-col items-center space-y-5 w-full">
+                    {finishes.options.map((finish) => (
+                      <Label
+                        key={finish.label}
+                        htmlFor={finish.value}
+                        className={cn(
+                          "relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border border-zinc-200 sm:flex sm:justify-between w-full",
+                          {
+                            "border-brand-primary":
+                              selectedFinish.value === finish.value,
+                          }
+                        )}
+                      >
+                        <div className="flex flex-col text-sm">
+                          <span className="font-medium text-gray-900">
+                            {finish.label}
+                          </span>
+                          {finish.description && (
+                            <span className="text-gray-500">
+                              {finish.description}
+                            </span>
+                          )}
+                        </div>
+                        <span className="text-sm font-medium text-gray-900">
+                          {formatPrice(finish.price)}
+                        </span>
+                        <RadioGroupItem
+                          value={finish.value}
+                          id={finish.value}
+                          className=" sr-only"
+                        />
+                      </Label>
+                    ))}
+                    <Label className="text-start"> Total price: {formatPrice(basePrice + selectedMaterial.price + selectedFinish.price)}</Label>
+                    <FormButton className="w-full btn btn-primary !py-3 !rounded-md">Continue</FormButton>
+                  </div>
                 </RadioGroup>
               </div>
             </div>
